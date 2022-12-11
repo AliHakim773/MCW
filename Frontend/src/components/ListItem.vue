a0d45a3fdd4d4bd63ae9710f2195283a802f99ce
AliHakim773 <93867233+AliHakim773@users.noreply.github.com>
2022-12-11 16:50:18 +0200

    added ListItem

---
+++ Frontend/src/components/ListItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="list-item">
+    <div class="list-cell" style="width: 5%">{{ number }}</div>
+    <div class="list-cell list-cell-img" style="width: 10%">
+      <img src="../../public/Lissa.png" class="list-img" :alt="title" />
+    </div>
+    <div class="list-cell list-cell-title" style="width: 65%">{{ title }}</div>
+    <div class="list-cell" style="width: 10%">{{ score }}</div>
+    <div class="list-cell" style="width: 10%"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ListItem',
+  props: ['number', 'title', 'score', 'deletable'],
+}
+</script>
+<style scoped>
+.list-item {
+  display: flex;
+}
+.list-cell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.3em;
+  font-weight: 400;
+  color: var(--color-black);
+}
+.list-cell:not(:first-child) {
+  border-left: 1px solid var(--color-grey);
+}
+.list-img {
+  height: 7.5rem;
+  width: 6rem;
+  object-fit: cover;
+}
+.list-cell-img {
+  padding: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.list-cell-title {
+  display: flex;
+  justify-content: flex-start;
+  padding-left: 2rem;
+}
+</style>
--- Frontend/src/views/ListView.vue
+++ Frontend/src/views/ListView.vue
@@ -6,19 +6,37 @@
     </header>
     <div class="div-comp-white list">
       <header>
-        <div class="category"><Categories :list="ListCategory" /></div>
-        <div class="list-header"></div>
+        <div class="category">
+          <div class="current-list-title">{{ CurrentList }}</div>
+          <Categories :list="ListCategory" />
+        </div>
+        <div class="list-nav">
+          <div class="list-title" style="width: 5%">#</div>
+          <div class="list-title" style="width: 10%">Image</div>
+          <div class="list-title" style="width: 65%">Title</div>
+          <div class="list-title" style="width: 10%">score</div>
+          <div class="list-title" style="width: 10%"></div>
+        </div>
+        <ListItem
+          :number="number"
+          :score="score"
+          :title="title"
+          :deletable="deletable"
+          class="list-items"
+        />
       </header>
     </div>
   </div>
 </template>
 <script>
 import Categories from '@/components/Categories.vue'
+import ListItem from '@/components/ListItem.vue'
 
 export default {
   name: 'ListView',
   components: {
     Categories,
+    ListItem,
   },
   data() {
     return {
@@ -27,6 +45,11 @@ export default {
         // { id: 1, listName: 'Marvel' },
         // { id: 2, listName: 'roror' },
       ],
+      CurrentList: 'Current List Name',
+      number: 1,
+      title: 'Title',
+      score: '8',
+      deletable: false,
     }
   },
 }
@@ -42,7 +65,8 @@ export default {
   font-weight: 600;
 }
 .list {
-  margin: 1rem 10rem 10rem 10rem;
+  margin: 1rem 7rem 7rem 7rem;
+  padding: 0.4rem;
 }
 .list header {
   display: flex;
@@ -50,6 +74,29 @@ export default {
 }
 .category {
   display: flex;
-  justify-content: end;
+  justify-content: space-between;
+}
+.current-list-title {
+  padding: 0.7rem;
+  font-size: 1.5em;
+  font-weight: 400;
+  color: var(--color-black);
+}
+.list-nav {
+  display: flex;
+}
+.list-items:not(:last-child) {
+  border-bottom: 1px solid var(--color-grey);
+}
+.list-title {
+  text-align: center;
+  padding-bottom: 0.4rem;
+  font-size: 1.3em;
+  font-weight: 400;
+  color: var(--color-black);
+  border-bottom: 1px solid var(--color-grey);
+}
+.list-title:not(:first-child) {
+  border-left: 1px solid var(--color-grey);
 }
 </style>
